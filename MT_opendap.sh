f58061722a91949a24c325843c89ac01990fe6ac
nigelrees <nigel.rees@tutanota.com>
2018-05-18 15:09:23 +1000

    adding new files

---
+++ MT_opendap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module purge
+module load python/2.7.11
+module load ipython/4.2.0-py2.7
+module load pandas/0.18.1-py2.7
+module load netcdf4-python/1.2.4-ncdf-4.3.3.1-py2.7
+module load virtualenv/py2.7
+module load birrp
+module load snuffler
+module load ascii2mseed
---
+++ MT_tutorial.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+module purge
+module load python/2.7.11
+module load ipython/4.2.0-py2.7
